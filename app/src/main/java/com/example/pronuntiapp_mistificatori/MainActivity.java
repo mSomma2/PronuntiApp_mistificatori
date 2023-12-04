cadd3640e7ba78e4cd11c415ff8a296f7db96ab7
Mistificapek <p.theofanoustudenti.uniba.it>
2023-12-04 11:54:35 +0100

    ecco a voi

--- app/src/main/java/com/example/pronuntiapp_mistificatori/MainActivity.java
+++ app/src/main/java/com/example/pronuntiapp_mistificatori/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
                         // Salva le coordinate iniziali del tocco
                         dX = view.getX() - event.getRawX();
                         dY = view.getY() - event.getRawY();
-                        dX=1;
+                        dX=0;
                         break;
 
                     case MotionEvent.ACTION_MOVE:
