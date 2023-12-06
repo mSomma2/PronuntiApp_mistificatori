2b76018b3ef9ac1070d845384792f1b34502b0bc
mSomma2 <m.somma19@studenti.uniba.it>
2023-12-06 18:44:10 +0100

    creazione

--- app/src/main/java/com/example/pronuntiapp_mistificatori/MainActivity.java
+++ app/src/main/java/com/example/pronuntiapp_mistificatori/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.pronuntiapp_mistificatori;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        // Verifica lo stato di accesso
+        if (!isLoggedIn()) {
+            // Se non è autenticato, avvia l'activity di login
+            startLoginActivity();
+            // Chiudi questa activity per evitare che l'utente torni indietro
+            finish();
+        }
+    }
+
+    private boolean isLoggedIn() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        return user != null;
+    }
+
+    // Metodo per avviare l'activity di login
+    private void startLoginActivity() {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 
 }
