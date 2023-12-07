919383a7efd802803474f88e28dbec9d48fc4cf3
mSomma2 <m.somma19@studenti.uniba.it>
2023-12-07 13:25:44 +0100

    creazione

--- app/src/main/java/com/example/pronuntiapp_mistificatori/Login.java
+++ app/src/main/java/com/example/pronuntiapp_mistificatori/Login.java
@@ -4,52 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
-    private float dX, dY;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        ImageView img;
-
-        img = findViewById(R.id.imageView);
-
-        img.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        // Salva le coordinate iniziali del tocco
-                        dX = view.getX() - event.getRawX();
-                        dY = view.getY() - event.getRawY();
-                        break;
-
-                    case MotionEvent.ACTION_MOVE:
-                        // Aggiorna la posizione dell'ImageView durante lo spostamento
-                        view.animate()
-                                .x(event.getRawX() + dX)
-                                .y(event.getRawY() + dY)
-                                .setDuration(0)
-                                .start();
-                        break;
-
-                    default:
-                        return false;
-                }
-                return true;
-            }
-        });
     }
 
-    private void goToSignUp(){
-        Intent i = new Intent(this, signUp.class);
+    public void goToSignUp(View view) {
+        Intent i = new Intent(this, singUp.class);
+        startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/example/pronuntiapp_mistificatori/singUp.java
+++ app/src/main/java/com/example/pronuntiapp_mistificatori/singUp.java
@@ -1,14 +1,149 @@
 package com.example.pronuntiapp_mistificatori;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class singUp extends AppCompatActivity {
+    EditText mail, password1, password2;
+    TextView ErrorMail, ErrorPassword1, ErrorPassword2;
+    Button SignUp;
+    boolean m = false, p1 = false, p2 = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
+        mail = findViewById(R.id.editTextEmail);
+        password1 = findViewById(R.id.editTextPassword);
+        password2 = findViewById(R.id.editTextRepeatPassword);
+        SignUp = findViewById(R.id.SignUp);
+
+        ErrorMail = findViewById(R.id.errorEmail);
+        ErrorPassword1 = findViewById(R.id.errorPassword1);
+        ErrorPassword2 = findViewById(R.id.errorPassword2);
+
+        //controllo campo email
+        mail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String emailInput = s.toString();
+
+                if (android.util.Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+                    // L'indirizzo email è valido, esegui le azioni desiderate
+                    m = true;
+                    ErrorMail.setVisibility(View.GONE);
+                    if(m && p1 && p2){
+                        SignUp.setEnabled(true);
+                        SignUp.setBackgroundColor(ContextCompat.getColor(singUp.this, R.color.blue));
+                    }
+                } else {
+                    // L'indirizzo email non è valido, gestisci di conseguenza
+                    m=false;
+                    ErrorMail.setVisibility(View.VISIBLE);
+                    SignUp.setEnabled(false);
+                    SignUp.setBackgroundColor(ContextCompat.getColor(singUp.this, R.color.red));
+                }
+            }
+        });
+
+        password1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String passwordInput = s.toString();
+                String passwordPattern = "^(?=.*[0-9])(?=.*[A-Z]).*$";
+
+                if (passwordInput.matches(passwordPattern)) {
+                    //la password contiene un numero e una maiuscola
+                    p1 = true;
+                    ErrorPassword1.setVisibility(View.GONE);
+                    if(m && p1 && p2){
+                        SignUp.setEnabled(true);
+                        SignUp.setBackgroundColor(ContextCompat.getColor(singUp.this, R.color.blue));
+                    }
+                } else {
+                    p1=false;
+                    ErrorPassword1.setVisibility(View.VISIBLE);
+                    SignUp.setEnabled(false);
+                    SignUp.setBackgroundColor(ContextCompat.getColor(singUp.this, R.color.red));
+                }
+            }
+        });
+
+        password2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String password1Input = password1.getText().toString();
+                String password2Input = s.toString();
+
+                if (password1Input.equals(password2Input)) {
+                    // Le password coincidono, esegui le azioni desiderate
+                    p2 = true;
+                    ErrorPassword2.setVisibility(View.GONE);
+                    if(m && p1 && p2){
+                        SignUp.setEnabled(true);
+                        SignUp.setBackgroundColor(ContextCompat.getColor(singUp.this, R.color.blue));
+                    }
+                } else {
+                    // Le password non coincidono, gestisci di conseguenza
+                    p2=false;
+                    ErrorPassword2.setVisibility(View.VISIBLE);
+                    SignUp.setEnabled(false);
+                    SignUp.setBackgroundColor(ContextCompat.getColor(singUp.this, R.color.red));
+                }
+            }
+        });
+    }
+
+    public void signUp(View view) {
+        String mailS, pw1, pw2;
+        mailS = String.valueOf(mail.getText());
+        pw1 = String.valueOf(password1.getText());
+        pw2 = String.valueOf(password2.getText());
+        finish();
+    }
+
+    public void goToLogIn(View view) {
+        Intent i = new Intent(this, Login.class);
+        startActivity(i);
+        finish();
     }
 }
