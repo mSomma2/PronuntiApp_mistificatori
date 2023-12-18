06827e1baffba27eb94c7a08e7173c0280db6c34
gneglia6 <g.neglia6@studenti.uniba.it>
2023-12-18 12:01:28 +0100

    Creazione collegamento tra Parentmenu e bambino_activity.xml

--- app/src/main/java/com/example/pronuntiapp_mistificatori/Parentmenu.java
+++ app/src/main/java/com/example/pronuntiapp_mistificatori/Parentmenu.java
@@ -1,12 +1,17 @@
 package com.example.pronuntiapp_mistificatori;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Parentmenu extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,17 @@ public class Parentmenu extends AppCompatActivity {
         code = findViewById(R.id.kidCode);
         name.setText(nome);
         code.setText(codice);
+
+        //bottone Inizia Gioco
+        Button iniziaGiocoButton = findViewById(R.id.iniziaGiocoButton);
+        iniziaGiocoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Parentmenu.this, bambino_activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     public void background(View view) {
